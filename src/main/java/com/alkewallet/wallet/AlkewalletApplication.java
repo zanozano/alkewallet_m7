ab04924a20010ce2866825d15a743a3190bb3f59
zano <zano@live.cl>
2024-06-07 20:24:48 +0700

    update

---
+++ src/main/java/com/alkewallet/wallet/AlkewalletApplication.java
@@ -0,0 +1,13 @@
+package com.alkewallet.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkewalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkewalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkewallet/wallet/AlkewalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkewallet.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkewalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
