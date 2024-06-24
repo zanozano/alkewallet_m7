a27c15f57ba52ca7d834e58a1535b9a6ea2aba03
zano <zano@live.cl>
2024-06-24 15:30:58 +0700

    update

--- src/main/java/com/alkewallet/wallet/AlkewalletApplication.java
+++ src/main/java/com/alkewallet/wallet/AlkewalletApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AlkewalletApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(AlkewalletApplication.class, args);
 	}
